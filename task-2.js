5c86ca7b3e00b719ee314d9238a0442baf67821c
lukasz-sklad <lukasz.sklad@gmail.com>
2024-08-04 21:09:01 +0200

    Rozwiazania

--- task-2.js
+++ task-2.js
@@ -1,4 +1,20 @@
 'use strict'
+class Storage {
+	#items // Prywatna właściwość przechowująca towary
+
+	constructor(initialItems) {
+		this.#items = initialItems // Inicjalizacja prywatnej właściwości wartością początkową
+	}
+	getItems() {
+		return this.#items // Zwraca aktualną listę towarów
+	}
+	addItem(newItem) {
+		this.#items.push(newItem) // Dodaje nowy towar do listy
+	}
+	removeItem(itemToRemove) {
+		this.#items = this.#items.filter(item => item !== itemToRemove) // Usuwa towar z listy
+	}
+}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
--- task-3.js
+++ task-3.js
@@ -1,4 +1,23 @@
 'use strict'
+class StringBuilder {
+	#value // Prywatna właściwość przechowująca ciąg znaków
+
+	constructor(initialValue) {
+		this.#value = initialValue // Inicjalizacja prywatnej właściwości wartością początkową
+	}
+	getValue() {
+		return this.#value // Zwraca aktualną wartość ciągu znaków
+	}
+	padStart(str) {
+		this.#value = str + this.#value // Dodaje ciąg znaków na początku wartości
+	}
+	padEnd(str) {
+		this.#value = this.#value + str // Dodaje ciąg znaków na końcu wartości
+	}
+	padBoth(str) {
+		this.#value = str + this.#value + str // Dodaje ciąg znaków na początku i na końcu wartości
+	}
+}
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
